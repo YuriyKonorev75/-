7003df0b772d44e75b95764817d2cab250d5157a
Yuriy <yuriy.konorev@normet.com>
2022-08-05 17:57:08 +0600

    add

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("020202 022Hello, World!");
+﻿// Рисунок случайным выбором
+Console.WriteLine("Рисуем случайным образом");
+int xa = 1; ya = 1;
+int xb = 1; yb = 60;
+int xc = 90; yc = 90;
+console.cursorposition(xa,ya);console.WriteLine("+");
+int x =xa; y = ya;
+int count =0;
+while (count<1000)
+{
+    int what = new Random().Next(1,90);
+    if (what == 0);
+    {
+    x=(x+xa)/2
+    y=(y+ya)/2 
+    }
+    
+}
+int what = new Random().Next(1,90);
+    if what == 0;
+    x=(x+xb)/2
+    y=(y+yb)/2
